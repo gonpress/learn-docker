3469f35dd60e7dc6aa1d09e3eeb191800a77c7d5
gonpress <gonpress@naver.com>
2022-04-02 10:09:05 +0900

    add db connect

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConfig";
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
